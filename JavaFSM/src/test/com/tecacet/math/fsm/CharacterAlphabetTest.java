c3e21827024b65e82e284e9fc3a527f0452e0689
papajim <papajim@f9bda04b-fc24-4a6f-bc4b-8a1a375f9f73>
2012-05-21 14:25:43 +0000

    Bad

--- JavaFSM/src/test/com/tecacet/math/fsm/CharacterAlphabetTest.java
+++
@@ -1,9 +0,0 @@
-package com.tecacet.math.fsm;
-
-public interface MealyMachine<S,CI,CO> extends DeterministicFiniteAutomaton<S,CI> {
-
-    Alphabet<CO> getOutputAlphabet();
-
-    CO getOutput(S q, CI c);
-
-}

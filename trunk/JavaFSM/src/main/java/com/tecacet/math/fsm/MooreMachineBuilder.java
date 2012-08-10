894aec8a35ebf93376c760b3f0b9e97a6bcf996a
papajim <papajim@f9bda04b-fc24-4a6f-bc4b-8a1a375f9f73>
2012-08-10 19:28:27 +0000

    added simple Moore Machine

--- trunk/JavaFSM/src/main/java/com/tecacet/math/fsm/MooreMachineBuilder.java
+++ trunk/JavaFSM/src/main/java/com/tecacet/math/fsm/MooreMachineBuilder.java
@@ -1,6 +1,9 @@
 package com.tecacet.math.fsm;
 
-public interface MooreMachineBuilder<S,CI, CO> extends DFABuilder<S, CI>{
+public interface MooreMachineBuilder<S, CI, CO> extends DFABuilder<S, CI> {
 
-    void addOutput(S state, CO output);
+	void addOutput(S state, CO output);
+
+	@Override
+	MooreMachine<S, CI, CO> build() throws DFABuilderException;
 }
